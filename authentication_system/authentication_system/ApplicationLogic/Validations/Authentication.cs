cbd3a63f325e11b27a9f1495d03e404e1874da4a
elnurmehdi <elnurkm@code.edu.az>
2022-07-17 23:35:00 +0400

    GetName, GetSurname methods added to Authentication class.

--- authentication_system/authentication_system/ApplicationLogic/Validations/Authentication.cs
+++ authentication_system/authentication_system/ApplicationLogic/Validations/Authentication.cs
@@ -8,5 +8,40 @@ namespace authentication_system.ApplicationLogic.Validations
 {
     class Authentication
     {
+        public static void Register()
+        {
+
+
+        }
+
+
+
+        public static string GetName()
+        {
+            Console.Write("Please, enter user's name: ");
+            string name = Console.ReadLine();
+
+            while (!Validation.IsValidName(name))
+            {
+                Console.Write("Please, enter correct user name: ");
+                name = Console.ReadLine();
+            }
+            return name;
+        }
+
+        public static string GetSurname()
+        {
+            Console.Write("Please, enter user's surname: ");
+            string surname = Console.ReadLine();
+
+            while (!Validation.IsValidSurname(surname))
+            {
+                Console.Write("Please, enter correct user surname: ");
+                surname = Console.ReadLine();
+            }
+            return surname;
+
+        }
     }
+
 }
