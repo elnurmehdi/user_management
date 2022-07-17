0beb83306f3c2d80924dcc7e2f329bca8b543ab6
elnurmehdi <elnurkm@code.edu.az>
2022-07-17 23:37:50 +0400

    GetEmail, GetPassword methods added to Authentication class.

--- authentication_system/authentication_system/ApplicationLogic/Validations/Authentication.cs
+++ authentication_system/authentication_system/ApplicationLogic/Validations/Authentication.cs
@@ -42,6 +42,41 @@ namespace authentication_system.ApplicationLogic.Validations
             return surname;
 
         }
+
+
+        public static string GetEmail()
+        {
+            Console.Write("Please, enter user's email: ");
+            string email = Console.ReadLine();
+
+            while (!Validation.IsValidEmail(email))
+            {
+                Console.Write("Please, enter correct user email: ");
+                email = Console.ReadLine();
+            }
+            return email;
+
+        }
+
+        public static string GetPassword()
+        {
+            Console.Write("Please, enter user's password: ");
+            string password = Console.ReadLine();
+            while (!Validation.IsValidPassword(password))
+            {
+                Console.Write("Please, enter correct user password: ");
+                password = Console.ReadLine();
+            }
+            Console.Write("Please, enter password again: ");
+            string confirmPassword = Console.ReadLine();
+            while (!Validation.IsPasswordConfirm(password, confirmPassword))
+            {
+                Console.Write("Please, enter correct confirm password: ");
+                confirmPassword = Console.ReadLine();
+            }
+            return password;
+
+        }
     }
 
 }
